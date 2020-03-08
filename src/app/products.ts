9a081e9b2a83dce3fec3a6910d876f29108ecb18
Viniciustertuliano <41974347+Viniciustertuliano@users.noreply.github.com>
2020-03-07 22:53:20 -0300

    Resolução de bugs, construção do top bar e lista de produtos

---
+++ src/app/products.ts
@@ -0,0 +1,17 @@
+export const products = [
+    {
+        name: 'Phone XL',
+        price: 799,
+        description: 'A large phone with one of the best screens'
+    },
+    {
+        name: 'Phone mini',
+        price: 699,
+        description: 'A great phone with one of the best camera'
+    },
+    {
+        name: 'Phone Standart',
+        price: 299,
+        description: ''
+    }
+];
